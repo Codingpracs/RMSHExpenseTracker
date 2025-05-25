90e050f0df3e4bad23d483ae51aa85d49330234c
Your Name <cqydarren@gmail.com>
2025-05-26 02:15:51 +0800

    added update function

--- main.py
+++ main.py
@@ -50,8 +50,29 @@ def delete(database, id) -> None:
 
 
 # args means can determine what the argument represents depending on the input count/ type
-def update(id,*args):
-    pass
+def update(database,id,*args):
+    description = None
+    amount = None
+
+    if len(args) == 1:
+        if isinstance(args[0], str):
+            description = args[0]
+        elif isinstance(args[0], (int,float)):
+            amount = args[0]
+    elif len(args) == 2:
+        description = args[0]
+        amount = args[1]
+    
+    database['description'] = description
+    database['amount'] = amount
+    viewlist({id: database[id]})
+
+    if amount is not None:
+        print(f"Amount of expense {id} has changed to {amount}")
+    if description is not None:
+        print(f"Description of expense {id} has changed to {description}")
+    if amount is None and description is None:
+        print(f"Nothing has changed for expense {id}")
 
 def viewsummary(database):
     amt_log = []
@@ -72,4 +93,19 @@ def viewlist(database) -> None:
         print(f"{id:<5} {properties['Date']:<15} {properties['Description']:<30} {properties['Amount']:<20}")
 
 def main():
-    pass
+    parser = ArgumentParser(description = "expense tracker")
+    subparsers = parser.add_subparsers(dest = 'command', required = True)
+    
+    add_parser = subparsers.add_parser("add", help = "Add an amount n description")
+    add_parser.add_argument('description', help = 'Description of item')
+    add_parser.add_argument('amount', help = 'Amt spent')
+    add_parser.set_defaults(func = add)
+
+    del_parser = subparsers.add_parser("delete")
+    del_parser.add_argument("id")
+    del_parser.set_defaults(func = delete)
+
+    update_parser = subparsers.add_parser("update")
+    # change is regardless of whether amount of description, this is settled in update function
+    update_parser.add_argument("change", choices = [])
+    update_parser.set_defaults(func = delete)
